9f78cfe0534cd097f2e4960ba3f3b3741585dc80
savvyh <7277@holbertonstudents.com>
2024-04-23 13:57:29 +0000

    Create a function that return a list of float or int.

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Create a function that return a list of float or int."""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return the sum of integers and floats in the mixed list."""
+    return sum(mxd_lst)
