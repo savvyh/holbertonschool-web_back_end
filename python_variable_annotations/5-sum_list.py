a5785a5721c2f0cb5d5eb68e116f5a8c8aad327a
savvyh <7277@holbertonstudents.com>
2024-04-23 13:53:40 +0000

    Create a function that return a list of float.

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Create a function that return a list of float."""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of floats in the input list."""
+    return sum(input_list)
