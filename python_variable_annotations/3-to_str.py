c7236e545e608eaf92d51673159262b33ed5ce25
savvyh <7277@holbertonstudents.com>
2024-04-23 13:47:05 +0000

    Create a function that return a string but with a float as argument

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Create a function that return a string but with a float as arg."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the given float."""
+    return str(n)
