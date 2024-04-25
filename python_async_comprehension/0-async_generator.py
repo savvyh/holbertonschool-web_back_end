32d7e6456a1b70c28f7ffd26941f98da96421e36
savvyh <7277@holbertonstudents.com>
2024-04-25 12:07:39 +0000

    Fix the code by adding a NonType

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async  generator."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Union, NoneType
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[Union[float, None], NoneType]:
     """Coroutine that yields a random number after waiting for 1 second."""
     for _ in range(10):
         await asyncio.sleep(1)
