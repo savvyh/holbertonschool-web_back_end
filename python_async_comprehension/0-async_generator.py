113447339141abdd9ef9615cf9e50692dc5c0564
savvyh <7277@holbertonstudents.com>
2024-04-25 11:51:45 +0000

    Fix the code by adding a float None

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async generator."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine that yields a random number after waiting for 1 second."""
     for _ in range(10):
         await asyncio.sleep(1)
