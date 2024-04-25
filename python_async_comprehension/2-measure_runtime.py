dbeca0469f94c49d238cedb038ca4265d976d51a
savvyh <7277@holbertonstudents.com>
2024-04-25 11:57:13 +0000

    Fix the code to run time 4 parallel comprehensions.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Run time 4 parallel comprehensions."""
 import asyncio
-import time
+from time import perf_counter
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -9,16 +9,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Asynch coroutine that will execute async_comprehension
     4 times in parallel using asyncio.gather, the return the running time."""
-    start_time = time.time()
+    start_time = perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    end_time = perf_counter()
+    total_runtime = end_time - start_time
 
-    end_time = time.time()
-    total_time = end_time - start_time
-
-    return total_time
+    return total_runtime
