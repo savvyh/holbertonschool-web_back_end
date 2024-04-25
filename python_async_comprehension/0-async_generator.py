fb559f0cb34fb24f229b76c52a552eb6bc15fe94
savvyh <7277@holbertonstudents.com>
2024-04-25 12:08:20 +0000

    Fix the code by changing the order

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,11 @@
 """Async  generator."""
 import asyncio
 import random
+from types import NoneType
 from typing import AsyncGenerator, Union, NoneType
 
 
-async def async_generator() -> AsyncGenerator[Union[float, None], NoneType]:
+async def async_generator() -> AsyncGenerator[float, Union[None, NoneType]]:
     """Coroutine that yields a random number after waiting for 1 second."""
     for _ in range(10):
         await asyncio.sleep(1)
