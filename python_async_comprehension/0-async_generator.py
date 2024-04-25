c281dfd7b1050b2ee7cfd1dd125f4ac42f7d80ee
savvyh <7277@holbertonstudents.com>
2024-04-25 10:42:46 +0000

    Create a async generator with Coroutine that yields a random number after waiting for 1 second.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async generator."""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Coroutine that yields a random number after waiting for 1 second."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
