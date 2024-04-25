a8cd9ff0b7c1b980be50746b6099aa950d87c369
savvyh <7277@holbertonstudents.com>
2024-04-25 12:10:47 +0000

    Come back to my previous code because I lost point

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,12 +2,10 @@
 """Async  generator."""
 import asyncio
 import random
-from typing import AsyncGenerator
-from types import NoneType
 
 
-async def async_generator() -> AsyncGenerator[float, NoneType]:
+async def async_generator():
     """Coroutine that yields a random number after waiting for 1 second."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform(0, 10) 
