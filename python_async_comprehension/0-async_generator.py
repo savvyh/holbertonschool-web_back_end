bb26845eb4944ab693a8a34a538ca5d4cfd5f68a
savvyh <7277@holbertonstudents.com>
2024-04-26 09:24:42 +0000

    Update the code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,9 @@ import asyncio
 import random
 
 
+
 async def async_generator():
     """Coroutine that yields a random number after waiting for 1 second."""
-    for _ in range(10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
