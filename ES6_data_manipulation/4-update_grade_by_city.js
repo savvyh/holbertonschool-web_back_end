ff623753285afe6b33e65ac290ff5bb472730ad4
savvyh <7277@holbertonstudents.com>
2024-05-24 07:44:46 +0000

    Update the grade value

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,7 +6,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 
       return {
         ...student,
-        grade: newGrade ? newGrade.grade : 'N/A',
+        grade: (newGrades.find((grade) => grade.studentId === student.id) || { grade: 'N/A' }).grade,
       };
     });
 }
