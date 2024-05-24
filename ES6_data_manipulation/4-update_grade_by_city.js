19dd5cdef762646c28e9651615ce663f260bbb10
savvyh <7277@holbertonstudents.com>
2024-05-24 07:48:28 +0000

    update the code by delete the wrong line

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.city === city)
-    .map((student) => {
-      const newGrade = newGrades.find((grade) => grade.studentId === student.id);
-
-      return {
-        ...student,
-        grade: (newGrades.find((grade) => grade.studentId === student.id) || { grade: 'N/A' }).grade,
-      };
-    });
+    .map((student) => ({
+      ...student,
+      grade: (newGrades.find((grade) => grade.studentId === student.id) || { grade: 'N/A' }).grade,
+    }));
 }
