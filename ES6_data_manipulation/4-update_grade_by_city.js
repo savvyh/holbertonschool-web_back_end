884a64a28101368589d74143d8f13665c4925c16
savvyh <7277@holbertonstudents.com>
2024-05-24 08:57:34 +0000

    Update the student location

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((student) => student.city === city)
+    .filter((student) => student.location === city)
     .map((student) => ({
       ...student,
       grade: (newGrades.find((grade) => grade.studentId === student.id) || { grade: 'N/A' }).grade,
