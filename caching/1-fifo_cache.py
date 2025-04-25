d7f16f958ad41ceed86a5d0b6c772309685ae571
savvyh <7277@holbertonstudents.com>
2025-04-25 21:23:54 +0000

    feat: add a method with a FIFOCache class that inherits from BaseCaching and is a caching system

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module for implementing a FIFO caching system
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache (BaseCaching):
+    """
+    FIFOCache class that inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discardElement = self.cache_data.pop(0)
+            print(f"DISCARD: {discardElement}")
+
+    def get(self, key):
+        """
+        Get an item in the cache
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
