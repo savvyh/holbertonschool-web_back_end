cd75f57b3f2f2267d811f402be03cc1c68b438a8
savvyh <7277@holbertonstudents.com>
2025-05-23 12:38:52 +0000

    fix: requirements maximum of 5 lines per function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,6 +10,4 @@ def filter_datum(fields, redaction, message, separator):
     """
     filter_datum function
     """
-
     return re.sub(r'{}'.format(fields, separator), redaction, message)
-    
