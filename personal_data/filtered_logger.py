e364fec7402dc32bd1dba9991e077ad609662eef
savvyh <7277@holbertonstudents.com>
2025-05-23 14:51:45 +0000

    feat: implement a get_logger function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ filtered logger module
 import re
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "address")
+
 
 class RedactingFormatter(logging.Formatter):
     """
@@ -33,3 +35,18 @@ class RedactingFormatter(logging.Formatter):
         filter_datum function
         """
         return re.sub(r'{}'.format(fields, separator), redaction, message)
+
+
+def get_logger():
+    """
+    get_logger function
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+
+    return logger
