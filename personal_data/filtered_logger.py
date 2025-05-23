5b61002fb89a92a9e3552949ea0ae1a6eb0faeca
savvyh <7277@holbertonstudents.com>
2025-05-23 15:15:55 +0000

    feat: create a get_db method to connect to secure database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ filtered logger module
 
 import re
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
 
@@ -57,3 +59,21 @@ def get_logger():
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db():
+    """
+    get_db function that returns a connector to the database
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+
