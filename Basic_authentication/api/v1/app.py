f0be26c85a494f0aeae075075e39041fd92b5455
savvyh <7277@holbertonstudents.com>
2025-06-06 12:34:08 +0000

    fix: use require_auth method to check if auth is needed

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -28,7 +28,7 @@ def before_request():
         return
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
+    if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
