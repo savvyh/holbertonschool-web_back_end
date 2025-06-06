4fed62f263abe508e92d80aecd734c28f8742c32
savvyh <7277@holbertonstudents.com>
2025-06-06 09:11:28 +0000

    feat: create a new error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,14 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - 401 error
+    """
+    return abort(401)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -24,3 +32,4 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
