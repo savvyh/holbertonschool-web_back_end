e8ad146e1d092d5f4b298b91751259767b4ad6fe
savvyh <7277@holbertonstudents.com>
2025-06-06 09:18:13 +0000

    feat: create a new error handler : forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -21,6 +21,13 @@ def unauthorized() -> str:
     """
     return abort(401)
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - 403 error
+    """
+    return abort(403)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
