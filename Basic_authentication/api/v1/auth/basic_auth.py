939c0093e2e9fd3a65c08d64e6ff9e1c0e9ef6f7
savvyh <7277@holbertonstudents.com>
2025-06-06 14:07:02 +0000

    feat: add new method in the class BasicAuth that returns the user email and password from the Base64 decoded value.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,14 @@ class BasicAuth(Auth):
             return decoded_header.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ extract_user_credentials method
+        """
+        if decoded_base64_authorization_header is None or \
+                not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        return decoded_base64_authorization_header.split(':')
