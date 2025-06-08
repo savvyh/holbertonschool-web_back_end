6617af09cad239f11ad13fa78a49f172d633897e
savvyh <7277@holbertonstudents.com>
2025-06-08 23:03:06 +0000

    feat: add a final method current_user with all pieces for having a complete Basic authentication.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -62,3 +62,15 @@ class BasicAuth(Auth):
             return users[0]
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method
+        """
+        authorization_header = self.authorization_header(request)
+        base64_authorization_header = self.extract_base64_authorization_header(
+            authorization_header)
+        decoded_base64 = self.decode_base64_authorization_header(
+            base64_authorization_header)
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_base64)
+        return self.user_object_from_credentials(user_email, user_pwd)
