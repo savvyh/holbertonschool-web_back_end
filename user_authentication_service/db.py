91a5d99a46674eae0ecc6c156321e9e01d9b09d5
savvyh <7277@holbertonstudents.com>
2025-06-23 11:39:48 +0000

    feat: implement new method to find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
@@ -36,3 +38,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except (InvalidRequestError, NoResultFound):
+            raise
