75434b1b96394b5986837001a494c1b7de06b8b3
savvyh <7277@holbertonstudents.com>
2025-06-23 11:54:42 +0000

    feat: implement new class Auth and handle register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,9 +3,35 @@
 Auth module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_password = _hash_password(password)
+
+        user = self._db.add_user(email, hashed_password.decode('utf-8'))
+
+        return user
