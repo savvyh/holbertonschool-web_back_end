056f6019d8f64973a32e3986f11a5c25da3a01fa
savvyh <7277@holbertonstudents.com>
2025-06-23 10:52:41 +0000

    feat: improve before_request method

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -43,11 +43,9 @@ def before_request():
         if auth.authorization_header(request) is None and \
                 auth.session_cookie(request) is None:
             abort(401)
-        if auth.authorization_header(request) is None:
-            abort(401)
         if auth.current_user(request) is None:
             abort(403)
-        request.current_user = auth.current_user(request)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
