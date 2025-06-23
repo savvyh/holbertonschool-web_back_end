2119007d533aa2c4b7ef64bf8b640bb651861159
savvyh <7277@holbertonstudents.com>
2025-06-23 17:16:39 +0000

    feat: add a route to update a user's password

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -71,5 +71,12 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
