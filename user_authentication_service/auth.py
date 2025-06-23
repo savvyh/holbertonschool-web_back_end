f00d2b01c203befc89f029dbd1df6a5b2d0078de
savvyh <7277@holbertonstudents.com>
2025-06-23 16:23:12 +0000

    feat: add new method to implement valid login

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -35,3 +35,13 @@ class Auth:
         user = self._db.add_user(email, hashed_password.decode('utf-8'))
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
