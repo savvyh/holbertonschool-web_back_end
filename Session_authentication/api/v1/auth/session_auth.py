400f146d97dbb44badf3523fe316289667066e9d
savvyh <7277@holbertonstudents.com>
2025-06-23 11:15:27 +0000

    feat: add a new method to delete user session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -39,3 +39,19 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None) -> bool:
+        """ Delete the user session / logout """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
