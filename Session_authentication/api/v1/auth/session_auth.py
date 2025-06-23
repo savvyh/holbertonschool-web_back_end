005a6776b59543196e77d4ca9b1ab13dd28bfb6b
savvyh <7277@holbertonstudents.com>
2025-06-23 10:55:05 +0000

    feat: update class by adding an instance method that returns a User instance based on a cookie value

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Session Authentication
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -23,3 +24,18 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """ Return a User instance based on a cookie value """
+        if request is None:
+            return None
+        
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        
+        return User.get(user_id)
