68f11bcd861ce8f64811937610ae991cd25437e0
savvyh <7277@holbertonstudents.com>
2025-06-23 17:23:55 +0000

    fix: fix password decode utf8 when updating

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -97,7 +97,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, hashed_password=hashed_password.decode('utf-8'))
             self._db.update_user(user.id, reset_token=None)
         except NoResultFound:
             raise ValueError
