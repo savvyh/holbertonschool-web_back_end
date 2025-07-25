04708c46625c75662d91b3ab340ed6a4a5e9c314
savvyh <7277@holbertonstudents.com>
2025-07-25 12:01:11 +0000

    feat:  modify get_local to params url app.

---
+++ i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel and locale parameter detection
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """Get the best matching locale from request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """Render the index page"""
+    return render_template('4-index.html')
