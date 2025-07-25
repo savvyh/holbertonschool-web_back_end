dd0fb1be5b609e963be4fccb1d6d70e44971da2f
savvyh <7277@holbertonstudents.com>
2025-07-25 11:04:57 +0000

    feat: add test for the class GithubOrgClient

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+A module for testing the client module.
+"""
+
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, Mock
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    A class for testing the GithubOrgClient class.
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test GithubOrgClient.org returns the correct value."""
+        url = f"https://api.github.com/orgs/{org_name}"
+        test_payload = {"name": org_name, "repos_url": url}
+        mock_get_json.return_value = test_payload
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        self.assertEqual(result, test_payload)
+        mock_get_json.assert_called_once_with(url)
