43fd70ae028cb0d690702700dcb969626f28a24a
savvyh <7277@holbertonstudents.com>
2025-07-25 11:45:33 +0000

    feat: add a Babel flask application  for local custom

---
+++ i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel and locale detection
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """Get the best matching locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """Render the index page"""
+    return render_template('2-index.html')
