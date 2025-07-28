702a6b0c4f6cd408458d1c3deadb3fb9667b87e7
savvyh <7277@holbertonstudents.com>
2025-07-28 10:26:14 +0000

    feat: add method to get Redis value and convert it to string integer or created fonction

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -28,3 +28,31 @@ class Cache:
         random_key = str(uuid.uuid1())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key, fn=None):
+        """
+        Get a value from Redis.
+        Args:
+            key: The key to get the value from.
+            fn: A function to convert the value to a specific type.
+        Returns:
+            The value of the key.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key):
+        """
+        Get a value from Redis and convert it to a string.
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key):
+        """
+        Get a value from Redis and convert it to an integer.
+        """
+        return self.get(key, int)
