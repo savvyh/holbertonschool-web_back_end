821618b80e053e862fd8b0473b78324417700b5f
savvyh <7277@holbertonstudents.com>
2025-07-29 06:46:31 +0000

    feat: add a method that display the history of calls of a particular function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -47,6 +47,29 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable):
+    """
+    Display the history of calls of a particular function.
+    """
+    key = method.__qualname__
+
+    count_calls = method.__self__._redis.get(key)
+    if count_calls is None:
+        print(f"Cache.store was called 0 times.")
+        return
+
+    count_calls = int(count_calls)
+    print(f"Cache.store {key} was called {count_calls} times:")
+
+    inputs = method.__self__._redis.lrange(f"{key}:inputs", 0, -1)
+    outputs = method.__self__._redis.lrange(f"{key}:outputs", 0, -1)
+
+    for input_data, output_data in zip(inputs, outputs):
+        input_str = input_data.decode('utf-8')
+        output_str = output_data.decode('utf-8')
+        print(f"{key}(*{input_str}) -> {output_str}")
+
+
 class Cache:
     """
     Cache class
