fe77a92705032de75879fe7e411bbe4541a75c8d
savvyh <7277@holbertonstudents.com>
2025-07-29 07:48:02 +0000

    fix: typing date error

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- MYSQL advanced project
 -- script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, IFNULL(split,2020) - IFNULL(formed,0) AS lifespan
+SELECT band_name, IFNULL(split, 2025) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
