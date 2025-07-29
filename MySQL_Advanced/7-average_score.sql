fa1fab461e0b6c3ff31416a1b9a55a12ac457446
savvyh <7277@holbertonstudents.com>
2025-07-29 08:38:23 +0000

    feat: add a new script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- MySQL Advanced project
+-- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id;
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+END$$
+DELIMITER ; 
