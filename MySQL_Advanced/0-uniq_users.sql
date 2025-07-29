3b9ef6a4ea88f0a4a9bdda4486445c9682610ac7
savvyh <7277@holbertonstudents.com>
2025-07-29 07:27:10 +0000

    fix: add comments above the script

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- MYSQL advanced project
+-- script that creates a users table with attributes id, email and name
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
