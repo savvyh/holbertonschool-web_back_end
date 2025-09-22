6e437f421ee0b9d13f222e6f07b0518b31aa5f66
savvyh <7277@holbertonstudents.com>
2025-09-22 12:19:27 +0000

    feat: create a basic Redis queuing system with a process to generate job and a second one to process it

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', function(error) {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', function(channel, message) {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
